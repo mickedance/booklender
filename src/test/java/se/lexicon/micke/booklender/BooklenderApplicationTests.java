29d5b6e307a8b7981ab6d4e0cfed7e52209fa099
mickedance <mickedance@hotmail.com>
2022-08-17 14:18:14 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/micke/booklender/BooklenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.micke.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
